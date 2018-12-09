74c2cba4199d88ea23fdfbff4bc3557517ac3b46
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-09 02:16:09 -0200

    C16 - Correções de Bugs com o Ponto

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -138,7 +138,7 @@ class CalcController {
                 this.pushOperation(value);
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
             }
@@ -151,6 +151,8 @@ class CalcController {
 
     addDot(){
         let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if(this.isOperator(lastOperation) || !lastOperation){
             this.pushOperation('0.')
