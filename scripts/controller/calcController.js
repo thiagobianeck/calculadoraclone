be93fb56731a997c3fc9c538f08723b8cde407c0
Thiago Bianeck <thiagobianeck@gmail.com>
2018-12-15 11:26:05 -0200

    C19 - Trabalhando com Áudio

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -20,7 +22,7 @@ class CalcController {
         document.addEventListener('paste', e=> {
             let text = e.clipboardData.getData('Text');
             this.displayCalc = parseFloat(text);
-        })
+        });
     }
 
     initialize(){
@@ -30,6 +32,23 @@ class CalcController {
         }, 1000);
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick', e=> {
+                this.toggleAudio();
+            })
+        });
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     copyToClipboard(){
@@ -43,6 +62,8 @@ class CalcController {
 
     initKeyboard(){
         document.addEventListener('keyup', e=>{
+
+            this.playAudio();
 
             switch (e.key){
                 case 'Escape':
@@ -229,6 +250,9 @@ class CalcController {
 
 
     execBtn(value){
+
+        this.playAudio();
+
         switch (value){
             case 'ac':
                 this.clearAll();
